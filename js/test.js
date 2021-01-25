0757e7ae0323eece7dba694e856b5ae181160183
gratrow96 <gratrow96@gmail.com>
2021-01-24 22:46:08 -0500

    more formatting index, resume, portfolio/content

--- js/test.js
+++ js/test.js
@@ -1,2 +1,3 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is: " + d + "</h1>";
+<object data="Grattan_Rowland_IV_Resume_1_6_2021_Software.pdf" type="application/pdf">
+    <embed src="Grattan_Rowland_IV_Resume_1_6_2021_Software.pdf" type="application/pdf" />
+</object>
